0bef3ec4ce704e011cc5244000e17e230b7f6611
matizzedigital <162776994+matizzedigital@users.noreply.github.com>
2024-03-09 18:08:59 -0300

    1.2

--- server.py
+++ server.py
@@ -25,61 +25,66 @@ def webhook():
     # Por exemplo, você pode extrair informações e tomar decisões com base nelas
 
     # Responda ao webhook (opcional)
-
     senderName = data["senderName"]
-    message = data["text"]["message"]
-    optionchoice = data["listResponseMessage"]["title"]
+    try:
+        message = data["text"]["message"]
+        if message == "AUTO":
+            wpp.sendText(f'{message}')
+            wpp.sendText(f'Olá, {senderName}. Bem vindo ao sistema de atendimento do nosso gestor de condomínios')
+            option = [
+                {
+                    "id": "1",
+                    "title": "Setor 1"
+                },
+                {
+                    "id": "2",
+                    "title": "Setor 2"
+                },
+                {
+                    "id": "3",
+                    "title": "Setor 3A"
+                },
+                {
+                    "id": "4",
+                    "tittle": "Setor 3B"
+                },
+                {
+                    "id": "5",
+                    "tittle": "Setor 4C"
+                },
+                {
+                    "id": "6",
+                    "tittle": "Setor 5"
+                },
+                {
+                    "id": "7",
+                    "tittle": "Setor 6"
+                }
+            ]
 
-    if message == "AUTO":
-        wpp.sendText(f'{message}')
-        wpp.sendText(f'Olá, {senderName}. Bem vindo ao sistema de atendimento do nosso gestor de condomínios')
-        option = [
-            {
-                "id": "1",
-                "title": "Setor 1"
-            },
-            {
-                "id": "2",
-                "title": "Setor 2"
-            },
-            {
-                "id": "3",
-                "title": "Setor 3A"
-            },
-            {
-                "id": "4",
-                "tittle": "Setor 3B"
-            },
-            {
-                "id": "5",
-                "tittle": "Setor 4C"
-            },
-            {
-                "id": "6",
-                "tittle": "Setor 5"
-            },
-            {
-                "id": "7",
-                "tittle": "Setor 6"
-            }
-        ]
+            wpp.sendOptionList(option)
+            if 1 <= message <= 70:
+                Casa = message
+                setor_usuario = Setor
+                casa_usuario = Casa
+                informacoes = api.buscar_informacoes(setor_usuario, casa_usuario)
 
-        wpp.sendOptionList(option)
+                if informacoes:
+                    wpp.sendText("Proprietário:", informacoes["Proprietário"])
+                    wpp.sendText("Rotas:", informacoes["Rotas"])
+                else:
+                    wpp.sendText("Informações não encontradas para o setor e casa fornecidos.")
+    except:
+        pass
 
-    if 'Setor' in optionchoice:
-        Setor = optionchoice
-        wpp.sendText(f'Beleza {senderName}, agora envie o número da casa:')
-    if 1 <= message <= 70:
-        Casa = message
-        setor_usuario = Setor
-        casa_usuario = Casa
-        informacoes = api.buscar_informacoes(setor_usuario, casa_usuario)
+    try:
+        optionchoice = data["listResponseMessage"]["title"]
 
-        if informacoes:
-            wpp.sendText("Proprietário:", informacoes["Proprietário"])
-            wpp.sendText("Rotas:", informacoes["Rotas"])
-        else:
-            wpp.sendText("Informações não encontradas para o setor e casa fornecidos.")
+        if 'Setor' in optionchoice:
+            Setor = optionchoice
+            wpp.sendText(f'Beleza {senderName}, agora envie o número da casa:')
+    except:
+        pass
     response = data
     return jsonify(response)
 if __name__ == '__main__':
